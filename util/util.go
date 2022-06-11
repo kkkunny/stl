dfe5ac4820ade2944ef80144c5ec3f7a6190a5e5
kkkunny <1401289859@qq.com>
2022-06-11 08:14:41 +0800

    增加c语言内存分配和释放函数

--- util/util.go
+++ util/util.go
@@ -1,10 +1,33 @@
 package util
 
-// 三元运算
-func Ternary[T any](c bool, t, f T)T{
-	if c{
+/*
+#include <stdio.h>
+*/
+import "C"
+import (
+	"errors"
+	"unsafe"
+)
+
+// Ternary 三元运算
+func Ternary[T any](c bool, t, f T) T {
+	if c {
 		return t
-	}else{
+	} else {
 		return f
 	}
+}
+
+// Alloc 分配内存
+func Alloc(size uintptr) (unsafe.Pointer, error) {
+	ptr := C.calloc(1, C.size_t(size))
+	if ptr == nil {
+		return nil, errors.New("heap memory alloc error")
+	}
+	return ptr, nil
+}
+
+// Free 释放内存
+func Free(ptr unsafe.Pointer) {
+	C.free(ptr)
 }
