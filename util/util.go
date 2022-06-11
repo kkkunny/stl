8b70fba37d1439a5d983f0917e27c59746e9e62f
kkkunny <1401289859@qq.com>
2022-06-11 08:45:27 +0800

    删除c语言内存分配和释放函数

--- util/util.go
+++ util/util.go
@@ -1,14 +1,5 @@
 package util
 
-/*
-#include <stdio.h>
-*/
-import "C"
-import (
-	"errors"
-	"unsafe"
-)
-
 // Ternary 三元运算
 func Ternary[T any](c bool, t, f T) T {
 	if c {
@@ -16,18 +7,4 @@ func Ternary[T any](c bool, t, f T) T {
 	} else {
 		return f
 	}
-}
-
-// Alloc 分配内存
-func Alloc(size uintptr) (unsafe.Pointer, error) {
-	ptr := C.calloc(1, C.size_t(size))
-	if ptr == nil {
-		return nil, errors.New("heap memory alloc error")
-	}
-	return ptr, nil
-}
-
-// Free 释放内存
-func Free(ptr unsafe.Pointer) {
-	C.free(ptr)
 }
