283c3fe03be82443d4e4b4995f1a4c094e780480
kkkunny <1401289859@qq.com>
2022-06-15 20:03:48 +0800

    增加json

--- util/util.go
+++ util/util.go
@@ -1,5 +1,10 @@
 package util
 
+import (
+	"bytes"
+	"encoding/json"
+)
+
 // Ternary 三元运算
 func Ternary[T any](c bool, t, f T) T {
 	if c {
@@ -7,4 +12,26 @@ func Ternary[T any](c bool, t, f T) T {
 	} else {
 		return f
 	}
+}
+
+// Json json序列化
+func Json(v any) ([]byte, error) {
+	var res bytes.Buffer
+	encoder := json.NewEncoder(&res)
+	err := encoder.Encode(v)
+	return res.Bytes(), err
+}
+
+// UnJson json反序列化
+func UnJson(v any, data []byte) error {
+	decoder := json.NewDecoder(bytes.NewReader(data))
+	return decoder.Decode(v)
+}
+
+// Must must
+func Must[T any](v T, e error) T {
+	if e != nil {
+		panic(e)
+	}
+	return v
 }
---
+++ util/util_test.go
@@ -0,0 +1,12 @@
+package util
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMust(t *testing.T) {
+	a := []int{1, 2, 3}
+	content := Must(Json(&a))
+	fmt.Println(string(content))
+}
