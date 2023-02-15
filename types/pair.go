c63862c7f50a1ac67ddc14c04f0f33ed8d85aabb
kkkunny <1401289859@qq.com>
2023-02-15 17:15:24 +0800

    add ThreePair

--- types/pair.go
+++ types/pair.go
@@ -10,3 +10,15 @@ type Pair[T, E any] struct {
 func NewPair[T, E any](f T, s E) Pair[T, E] {
 	return Pair[T, E]{First: f, Second: s}
 }
+
+// ThreePair 三组合
+type ThreePair[T1, T2, T3 any] struct {
+	First  T1
+	Second T2
+	Third  T3
+}
+
+// NewThreePair 新建组合
+func NewThreePair[T1, T2, T3 any](f T1, s T2, t T3) ThreePair[T1, T2, T3] {
+	return ThreePair[T1, T2, T3]{First: f, Second: s, Third: t}
+}
