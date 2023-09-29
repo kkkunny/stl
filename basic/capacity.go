b8645b9ce1ff0001bb8192da5656ffa6c13772d2
kkkunny <1401289859@qq.com>
2023-09-29 11:20:51 +0800

    增加basic包

---
+++ basic/capacity.go
@@ -0,0 +1,6 @@
+package stlbasic
+
+// Capacity 可获取容量的
+type Capacity interface {
+	Capacity() uint
+}
---
+++ basic/length.go
@@ -0,0 +1,6 @@
+package stlbasic
+
+// Length 可获取长度的
+type Length interface {
+	Length() uint
+}
