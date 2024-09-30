16ee41dd3aac01f034e2c8759e36109dfcb7d964
kkkunny <1401289859@qq.com>
2024-09-30 23:27:18 +0800

    add MapHash

---
+++ hash/maphash.go
@@ -0,0 +1,14 @@
+package stlhash
+
+import "github.com/dolthub/maphash"
+
+func GetMapHashFunc[T comparable]() func(v T) uint64 {
+	hasher := maphash.NewHasher[T]()
+	return func(vv T) uint64 {
+		return hasher.Hash(vv)
+	}
+}
+
+func MapHash[T comparable](v T) uint64 {
+	return GetMapHashFunc[T]()(v)
+}
