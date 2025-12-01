e619afec3a7a4fb19eaf1951d6138cb39af71e67
kkkunny <1401289859@qq.com>
2025-12-01 10:00:47 +0800

    feat: add os.Copy

--- os/file.go
+++ os/file.go
@@ -1,6 +1,9 @@
 package stlos
 
-import "os"
+import (
+	"io"
+	"os"
+)
 
 // Exist 是否存在文件
 func Exist(path string) (bool, error) {
@@ -11,4 +14,31 @@ func Exist(path string) (bool, error) {
 		return false, err
 	}
 	return true, nil
+}
+
+// Copy 拷贝文件和目录
+func Copy(from, to string) error {
+	fromInfo, err := os.Stat(from)
+	if err != nil {
+		return err
+	}
+
+	if fromInfo.IsDir() {
+		return os.CopyFS(to, os.DirFS(from))
+	}
+
+	fromFile, err := os.Open(from)
+	if err != nil {
+		return err
+	}
+	defer fromFile.Close()
+
+	toFile, err := os.Create(to)
+	if err != nil {
+		return err
+	}
+	defer toFile.Close()
+
+	_, err = io.Copy(toFile, fromFile)
+	return err
 }
